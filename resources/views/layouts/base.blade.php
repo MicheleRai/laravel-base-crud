df9b078f1ec96334ed003167447407c87f9fa869
MicheleRai <rainonemichele1999@gmail.com>
2023-01-20 16:05:48 +0100

    first commit

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fumetti</title>
+</head>
+<body>
+    @include('partials.header')
+    <div>
+        <h1>@yield('title')</h1>
+        <main>
+            @yield('content')
+        S</main>
+    </div>
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('home') }}">Fumetti</a>
+
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Cerca</button>
+            </form>
+        </div>
+    </div>
+</nav>
